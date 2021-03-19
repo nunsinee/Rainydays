e41b5902c9d5a226e9a1f3c345a8c9bb8a97d8c0
nunsinee <71260513+nunsinee@users.noreply.github.com>
2021-03-19 10:20:39 +0100

    update form css and js

--- js/checkout-page01-support.js
+++ js/checkout-page01-support.js
@@ -9,66 +9,50 @@ const phoneNumber = document.querySelector("#phoneNumber");
 const messageTwo = document.querySelector("#messageform");
 
 //1.1 if all the input  pass validation
-
-contactName.addEventListener("input", () => {
+function validateConfirmForm(event) {
+	event.preventDefault();
 	if (checkLength(contactName.value, 0) === true) {
 		contactNameError.style.display = "none";
 	} else {
 		contactNameError.style.display = "block";
 	}
-});
-
-address.addEventListener("input", () => {
 	if (checkLength(address.value, 9) === true) {
 		addressError.style.display = "none";
 	} else {
 		addressError.style.display = "block";
 	}
-});
-
-city.addEventListener("input", () => {
 	if (checkLength(city.value, 1) === true) {
 		cityError.style.display = "none";
 	} else {
 		cityError.style.display = "block";
 	}
-});
-
-country.addEventListener("input", () => {
 	if (checkLength(country.value, 1) === true) {
 		countryError.style.display = "none";
 	} else {
 		countryError.style.display = "block";
 	}
-});
-
-email.addEventListener("input", () => {
-	if (validateEmail(email.value) === true) {
-		emailError.style.display = "none";
-	} else {
-		emailError.style.display = "block";
-	}
-});
-
-postcode.addEventListener("input", () => {
 	if (validatePostcode(postcode.value) === true) {
 		postcodeError.style.display = "none";
 	} else {
 		postcodeError.style.display = "block";
 	}
-});
-
-phoneNumber.addEventListener("input", () => {
 	if (validatePhoneNumber(phoneNumber.value) === true) {
 		phoneNumberError.style.display = "none";
 	} else {
 		phoneNumberError.style.display = "block";
 	}
-});
+	if (validateEmail(email.value) === true) {
+		emailError.style.display = "none";
+	} else {
+		emailError.style.display = "block";
+	}
+}
 
+confirmForm.addEventListener("submit", validateConfirmForm);
 confirmForm.addEventListener("submit", submitForm);
 
-//2.1 if all the inputs  pass validation
+//2.filling address before payment
+//2.1 if all the input  pass validation
 
 function submitForm(event) {
 	event.preventDefault();
@@ -83,7 +67,7 @@ function submitForm(event) {
 	) {
 		//display the message when the form has been submitted
 		location.href =
-			"https://rainnydays-nunsinee.netlify.app/checkout-page02.html";
+			"https://infallible-wescoff-9c9edc.netlify.app/checkout-page02.html";
 		confirmForm.reset();
 	} else {
 		messageTwo.innerHTML = "";
