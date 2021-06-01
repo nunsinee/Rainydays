4762cf5c78c5727d7745b4e971d67422c6176d9e
nunsinee <71260513+nunsinee@users.noreply.github.com>
2021-06-01 10:49:32 +0200

    update css

--- js/scripts-api.js
+++ js/scripts-api.js
@@ -4,36 +4,36 @@ const womenJackets = document.querySelector(".women-jackets");
 const menJackets = document.querySelector(".men-jackets");
 
 async function getProducts() {
-	try {
-		const response = await fetch(url);
-		const getResults = await response.json();
-		console.log(getResults);
-		womenJackets.innerHTML = "";
-		menJackets.innerHTML = "";
-		createHTML(getResults);
-	} catch (error) {
-		console.log("An error occurred");
-	}
+    try {
+        const response = await fetch(url);
+        const getResults = await response.json();
+        console.log(getResults);
+        womenJackets.innerHTML = "";
+        menJackets.innerHTML = "";
+        createHTML(getResults);
+    } catch (error) {
+        console.log("An error occurred");
+    }
 }
 getProducts();
 
 function createHTML(products) {
-	products.forEach(function (product) {
-		if (product.categories[0].name === "men") {
-			menJackets.innerHTML += `<div>
+    products.forEach(function(product) {
+        if (product.categories[0].name === "men") {
+            menJackets.innerHTML += `<div>
 		<img src="${product.images[0].src}" alt="${product.name}">
 		<h3>${product.name} </h3>
 		<p> Price: <span class=\"woocommerce-Price-amount amount\"><bdi><span class=\"woocommerce-Price-currencySymbol\"></span>${product.price_html}</bdi></span> </p>
 		<div class="button"><a href="product.html?id=${product.id}"> View detail</a></div>
 		</div>`;
-		}
-		if (product.categories[0].name === "women") {
-			womenJackets.innerHTML += `<div>
+        }
+        if (product.categories[0].name === "women") {
+            womenJackets.innerHTML += `<div>
 		<img src="${product.images[0].src}" alt="${product.name}">
 		<h3>${product.name} </h3>
 		<p> Price: <span class=\"woocommerce-Price-amount amount\"><bdi><span class=\"woocommerce-Price-currencySymbol\"></span>${product.price_html}</bdi></span> </p>
 		<div class="button"><a href="product.html?id=${product.id}"> View detail</a></div>
 		</div>`;
-		}
-	});
+        }
+    });
 }
